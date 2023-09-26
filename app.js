78919d8c97fb7fa6b6c1e2924b070bdd60ab5466
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-09-26 09:27:26 +0100

    new changes

--- app.js
+++ app.js
@@ -1,58 +1,60 @@
-// problem domain: the Aunby Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
+const hours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
 
-// Each kitten's profile should have:
-// - name
-// - random age assigned // will do that later
-// - a list of what they like
-// - an image
-// - good with kids
-// - good with dogs
-// - good with other cats
-// - breed
+// give a random number between two numbers
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-const trevor = {
-  name: "Trevor",
-  age: 2,
-  interests: ["Philosophy", "Bacon", "Fish", "Minimilism"],
-  image: "./images/trevor.jpg",
-  goodWithKids: true,
-  goodWithDogs: false,
-  goodWithCats: false,
-  breed: "ginger",
+// create our first shop
+const seattle = {
+  location: "Seattle",
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesPerCust: 6.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    // add a random number to the customersPerHour array
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+    }
+  },
 };
 
-// we need to:
-// get (from the DOM) who the parent element is going to be. where am I attaching this new element
-const kittenProfiles = document.getElementById("kittenProfiles");
-
-// create a new element, or elements, that represent frankie
+const seattleSales = document.getElementById("seattleSales");
 const article = document.createElement("article");
 
-// inside that article, i need to put a h2 for the name, paragraph with their age, ul and some li's with their interests, image
-// name
 const h2 = document.createElement("h2");
-h2.textContent = trevor.name;
+h2.textContent = seattle.location;
 article.appendChild(h2);
 
-// age
-const p = document.createElement("p");
-p.textContent = `${trevor.name} is ${trevor.age} months old.`;
-article.appendChild(p);
-
-// interests
 const ul = document.createElement("ul");
-for (let i = 0; i < trevor.interests.length; i++) {
+for (let i = 0; i < hours.length; i++) {
   const li = document.createElement("li");
-  li.textContent = trevor.interests[i];
+  li.textContent = hours[i];
   ul.appendChild(li);
 }
 article.appendChild(ul);
 
-// image
-const img = document.createElement("img");
-img.src = trevor.image;
-img.setAttribute("alt", trevor.name);
-article.appendChild(img);
+seattle.calculateSales();
+console.log(seattle);
 
-// add the article to the page (by appending to the kittenProfiles div)
-kittenProfiles.appendChild(article);
+seattleSales.appendChild(article);
