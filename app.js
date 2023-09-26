f07deb8c137ff764e31590f8d83c33c22da40c98
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-09-26 11:59:51 +0100

    new changes

--- app.js
+++ app.js
@@ -30,31 +30,65 @@ const seattle = {
   cookiesPerHour: [],
   totalCookieSold: 0,
   calculateSales: function () {
-    // add a random number to the customersPerHour array
     for (let i = 0; i < hours.length; i++) {
       const randNum = randomNumber(this.minCust, this.maxCust);
       this.customersPerHour.push(randNum);
-      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+      this.cookiesPerHour.push(Math.floor(randNum * this.avgCookiesPerCust));
     }
   },
 };
 
-const seattleSales = document.getElementById("seattleSales");
-const article = document.createElement("article");
-
-const h2 = document.createElement("h2");
-h2.textContent = seattle.location;
-article.appendChild(h2);
-
-const ul = document.createElement("ul");
-for (let i = 0; i < hours.length; i++) {
-  const li = document.createElement("li");
-  li.textContent = hours[i];
-  ul.appendChild(li);
-}
-article.appendChild(ul);
+const tokyo = {
+  location: "Tokyo",
+  minCust: 3,
+  maxCust: 24,
+  avgCookiesPerCust: 1.2,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(Math.floor(randNum * this.avgCookiesPerCust));
+    }
+  },
+};
 
 seattle.calculateSales();
-console.log(seattle);
+tokyo.calculateSales();
 
-seattleSales.appendChild(article);
+// get the element on the page with the id salesData
+const salesData = document.getElementById("salesData");
+
+// add the title for the location
+const seattleH3 = document.createElement("h3");
+seattleH3.textContent = seattle.location;
+salesData.appendChild(seattleH3);
+
+// create a list to show the cookies sold at each hour
+const seattleUl = document.createElement("ul");
+// loop through out data and for each item create an <li>
+for (let i = 0; i < hours.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = `${hours[i]}: ${seattle.cookiesPerHour[i]} cookies`;
+  seattleUl.appendChild(li);
+}
+
+salesData.appendChild(seattleUl);
+
+// add the title for the location
+const tokyoH3 = document.createElement("h3");
+tokyoH3.textContent = tokyo.location;
+salesData.appendChild(tokyoH3);
+
+// create a list to show the cookies sold at each hour
+const tokyoUl = document.createElement("ul");
+// loop through out data and for each item create an <li>
+for (let i = 0; i < hours.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = `${hours[i]}: ${tokyo.cookiesPerHour[i]} cookies`;
+  tokyoUl.appendChild(li);
+}
+
+salesData.appendChild(tokyoUl);
